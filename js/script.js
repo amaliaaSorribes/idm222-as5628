377cfb6458de194fbdfc37a37990586ea059a35e
amaliaaSorribes <as5628@drexel.edu>
2025-03-20 11:18:17 +0100

    :art: Added transitions and small fixes
    -On image overlay in each art category the image and overlay smoothly appear
    -Both nav bar and buttons smoothly shade darker on hover

--- js/script.js
+++ js/script.js
@@ -11,22 +11,53 @@ function myFunction2() {
 }
 
 function openImageOverlay(event) {
-    const imgSrc = event.target.src;
+    let imgSrc = event.target.src;
     console.log("img src: ", imgSrc);
+    // let imgSrcParts = imgSrc.split('.');
+    // let newImgSrc = imgSrcParts[0]+'.jpg'
+    // console.log("img src 2: ", imgSrcParts)
 
     const overlay = document.getElementsByClassName('overlay')[0];
     overlay.style.display = 'block';
+    overlay.style.opacity = '0.25';
+    overlay.style.transition = 'opacity 0.25s ease-in';
+
+    setTimeout(() => {
+        overlay.style.opacity = '1';
+    }, 100); 
+
 
     const image = document.createElement('img');
-    image.src = imgSrc;
+    image.src = imgSrc; //newImgSrc;
     image.className = "imageOverlay";
+    // const right = document.createElement('button');
+    // right.className = "buttonNext"
+    image.style.opacity = '0.1'; 
+    image.style.transform = 'translate(-50%, -50%) scale(0.5)';
+    image.style.transition = 'opacity 0.5s ease, transform 0.3s ease 0.1s';
+
     const main = document.querySelector('main');
     main.appendChild(image);
+    //main.appendChild(right);
+
+    setTimeout(() => {
+        image.style.opacity = '1'; // Fade in
+        image.style.transform = 'translate(-50%, -50%) scale(1)'; 
+    }, 100); 
 }
 
 function closeImageOverlay() {
     const overlay = document.getElementsByClassName('overlay')[0];
-    overlay.style.display = 'none';
+    overlay.style.opacity = '1';
+    overlay.style.transition = 'opacity 0.5s ease-out';
+
+    setTimeout(() => {
+        overlay.style.opacity = '0';
+    }, 300);
+
+    setTimeout(() => {
+        overlay.style.display = 'none';
+    }, 400);
 
     const image = document.getElementsByClassName('imageOverlay')[0];
     const main = document.querySelector('main');
